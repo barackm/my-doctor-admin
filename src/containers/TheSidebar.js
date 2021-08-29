dba8091325f2e38a8dd070ce5d2239a2b555060c
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-29 09:52:01 +0200

    Initial commit

---
+++ src/containers/TheSidebar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  CCreateElement,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CSidebarNavDivider,
+  CSidebarNavTitle,
+  CSidebarMinimizer,
+  CSidebarNavDropdown,
+  CSidebarNavItem,
+} from "@coreui/react";
+
+import logo from "../assets/icons/logo.png";
+
+// sidebar nav config
+import navigation from "./_nav";
+
+const TheSidebar = () => {
+  const dispatch = useDispatch();
+  const show = useSelector((state) => state.sidebarShow);
+
+  return (
+    <CSidebar
+      show={show}
+      onShowChange={(val) => dispatch({ type: "set", sidebarShow: val })}
+    >
+      <CSidebarBrand
+        className="d-md-down-none"
+        to="/"
+        style={{ padding: "0.5rem 0" }}
+      >
+        <img src={logo} alt="logo" style={{ width: "70px", height: "50px" }} />
+        <h4 style={{ marginLeft: "1rem" }}>My Doctor</h4>
+      </CSidebarBrand>
+      <CSidebarNav>
+        <CCreateElement
+          items={navigation}
+          components={{
+            CSidebarNavDivider,
+            CSidebarNavDropdown,
+            CSidebarNavItem,
+            CSidebarNavTitle,
+          }}
+        />
+      </CSidebarNav>
+      <CSidebarMinimizer className="c-d-md-down-none" />
+    </CSidebar>
+  );
+};
+
+export default React.memo(TheSidebar);
---
+++ src/views/users/User.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CSwitch,
+  CButton,
+} from "@coreui/react";
+
+import CIcon from "@coreui/icons-react";
+
+import usersData from "./UsersData";
+
+const User = ({ match }) => {
+  const user = usersData.find((user) => user.id.toString() === match.params.id);
+  const userDetails = user
+    ? Object.entries(user)
+    : [
+        [
+          "id",
+          <span>
+            <CIcon className="text-muted" name="cui-icon-ban" /> Not found
+          </span>,
+        ],
+      ];
+
+  return (
+    <CRow>
+      <CCol lg={12}>
+        <CCard>
+          <CCardHeader
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            User id: {match.params.id}{" "}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CButton
+                size="sm"
+                className="btn-facebook btn-brand mr-1 mb-1"
+                to={`/users/${match.params.id}/edit`}
+              >
+                <CIcon size="sm" name="cil-pencil" />
+                <span className="mfs-2">Edit User</span>
+              </CButton>
+              <CButton
+                size="sm"
+                className="btn-pinterest btn-brand mr-1 mb-1"
+                to="/"
+              >
+                <CIcon size="sm" name="cil-trash" />
+                <span className="mfs-2">Remove User</span>
+              </CButton>
+            </div>
+          </CCardHeader>
+
+          <CCardBody>
+            <table className="table table-striped table-hover">
+              <tbody>
+                {userDetails.map(([key, value], index) => {
+                  return (
+                    <tr key={index.toString()}>
+                      <td>{`${key}:`}</td>
+                      <td>
+                        <strong>{value}</strong>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  );
+};
+
+export default User;
